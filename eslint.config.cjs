6eec133972a402d197da442be04abfc4d7fff73e
Serhan Çelik <serhann798@gmail.com>
2025-03-13 10:48:48 +0300

    eslint bug fix

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,12 +1,11 @@
-const { dirname } = require("path");
-const { fileURLToPath } = require("url");
-const { FlatCompat } = require("@eslint/eslintrc");
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: dirname(fileURLToPath(import.meta.url)), // Tek satırda tanımla
 });
 
-module.exports = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
